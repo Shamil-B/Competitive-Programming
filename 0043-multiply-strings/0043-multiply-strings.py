85c51283a2ecac2069fb59e1e01967997e712654
Shamil <77851940+Shamil-B@users.noreply.github.com>
2022-12-23 17:59:59 +0300

    Time: 218 ms (33.05%), Space: 13.9 MB (29.25%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,27 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        pdt = '0'
+        
+        for i in range(len(num1)-1,-1,-1):
+            tmp = '0'*(len(num1)-i-1)
+            rem = 0
+            
+            for j in range(len(num2)-1,-1,-1):
+                res = (int(num1[i])*int(num2[j])+rem)
+                rem = 0
+                
+                if j==0:
+                    tmp = str(res) + tmp
+                    
+                else:
+                    tmp = str(res)[-1] + tmp
+                
+                if res//10 != 0:
+                    rem = int(str(res)[0])
+                    
+            pdt = str(int(pdt)+int(tmp))
+
+        return pdt
+                
+        
+        
