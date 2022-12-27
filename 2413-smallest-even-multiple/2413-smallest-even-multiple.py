ced397816f17480646578caed2ce948d061b4415
Shamil <77851940+Shamil-B@users.noreply.github.com>
2022-12-27 18:21:53 +0300

    Time: 33 ms (84.98%), Space: 13.9 MB (49.41%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,20 +1,9 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        if n <= 2:
-            if n%2==0:
-                num = n
-            else:
-                num = n+1
-
-            while num%n!=0:
-                num += 2
-                
-            return num
-                
-        else:
-            while n%2!=0:
+        
+        while n%2!=0:
                 n+=n
                 
-            return n
+        return n
                 
         
