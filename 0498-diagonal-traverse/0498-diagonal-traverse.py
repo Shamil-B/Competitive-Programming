8109ca3d56a4bec2c5b0c5060b1f73c3a53d4257
Shamil <77851940+Shamil-B@users.noreply.github.com>
2022-12-29 19:53:22 +0300

    Time: 195 ms (93.08%), Space: 17.1 MB (91.35%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -8,7 +8,9 @@ class Solution:
         row,col = 0,0
         
         for i in range(m*n-1):
+            
             if upward:
+                
                 if col==n-1:
                     upward = False
                     row+=1
@@ -16,10 +18,12 @@ class Solution:
                 elif row==0:
                     upward = False
                     col+=1
+                    
                 else:
                     col+=1
                     row-=1
             else:
+                
                 if row==m-1:
                     upward = True
                     col+=1
@@ -27,11 +31,13 @@ class Solution:
                 elif col==0:
                     upward = True
                     row+=1
+                    
                 else:
                     col-=1
                     row+=1
 
             result.append(mat[row][col])
+            
         return result
     
     
