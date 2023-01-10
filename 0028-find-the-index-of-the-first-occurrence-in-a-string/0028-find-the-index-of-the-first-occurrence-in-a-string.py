3f4fdaab7b352414dcad6d4842d6af4a674c9c9e
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-01-10 18:46:05 +0300

    Time: 32 ms (84.65%), Space: 13.8 MB (63.22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,24 +1,35 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:        
-        size = len(needle)
+    def strStr(self, haystack: str, needle: str) -> int:
+        splitted = self.splitIt(haystack,len(needle))
+        
+        if needle in splitted:
+            return splitted[needle]
+        
+        return -1
+    
+    
+    def splitIt(self,string,size):
+        
+        index = 0
+        resDic = {}
         word = ''
         
-        for ind,ch in enumerate(haystack):
+        for ind,ch in enumerate(string):
             if ind < size-1:
                 word += ch
                 
             elif ind == size-1:
                 word += ch
-                if word == needle:
-                    return 0
-                
+                resDic[word] = index
+                index += 1
                 
             else:
                 word += ch
                 word = word[1:]
-                if word == needle:
-                    return ind - size + 1
-                                
+                if word not in resDic:
+                    resDic[word] = index
+                index += 1
                 
-        return -1
+                
+        return resDic
             
