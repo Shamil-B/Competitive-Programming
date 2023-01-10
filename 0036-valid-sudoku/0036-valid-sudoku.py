decf5aaff1518f4579626837416f9862cc389abc
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-01-10 18:00:46 +0300

    Time: 104 ms (76.33%), Space: 14 MB (33.24%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,51 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        boxes = [{} for i in range(9)]
+        
+        #check one row wise
+        for row in board:
+            
+            dic = {}
+            for num in row:
+
+                if num in dic:
+                    return False
+                
+                elif num != '.':
+                    dic[num] = 0
+                
+                
+        #check two column wise
+
+                    
+        for col in range(9):
+            
+            dic = {}
+            for row in range(9):
+                num = board[row][col]
+                
+                if num in dic:
+                    return False
+                
+                elif num != '.':
+                    dic[num] = 0
+                    
+                    ind = row - row%3
+                    ind = ind + col//3
+                    
+                    
+                    if num in boxes[ind]:
+                        return False
+                    
+                    else:
+                        boxes[ind][num] = 0
+                    
+                    
+        #check three box wise
+        
+        
+    
+
+                    
+        
+        return True
