9d8ad62579bb5e9623ed1bff0ca600ccb81c698d
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-01-17 09:56:54 +0300

    Time: 714 ms (97.61%), Space: 15.4 MB (69.06%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -4,7 +4,9 @@ class Solution:
         for i in range(len(arr)-2,-1,-1):
             tmp = arr[i]
             arr[i] = maxx
-            maxx = max(maxx,tmp)
+            
+            if tmp>maxx:
+                maxx = tmp
             
         arr[-1] = -1
         
