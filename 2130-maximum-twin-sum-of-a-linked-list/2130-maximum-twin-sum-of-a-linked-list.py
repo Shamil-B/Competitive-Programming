860f5ff50cd5949b378ce50417b59724d09a6ad4
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-02-21 11:41:06 +0300

    Time: 1440 ms (13.39%), Space: 54.6 MB (38.16%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -6,7 +6,6 @@
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         #The straightforward way
-        res = []
         n = 0
         cur = head
         while cur:
@@ -16,14 +15,41 @@ class Solution:
         cur = head
 
         for i in range(n):
-            if i < n//2:
-                res.append(cur.val)
-                
-            else:
-                res[n-i-1] += cur.val
-                
+            if i == n//2-1:
+                break
             cur = cur.next
-                
-        return max(res)
+            
+        cur.next = self.reverseList(cur.next)
+        
+        cur = cur.next
+        cur2 = head
+        maxx = 0
+        while cur:
+            maxx = max(maxx,cur.val+cur2.val)
+            cur = cur.next
+            cur2 = cur2.next
+            
+        return maxx
+    
+    def reverseList(self, head):
+        current = head
+        if not head:
+            return 
+            
+        elif head.next:
+            current = head.next 
+
+        else:
+            return head        
+        
+        new = ListNode(head.val)
+        
+        while current:
+            current2 = ListNode(current.val)
+            current2.next = new
+            new = current2
+            current = current.next
+
+        return current2
             
         
