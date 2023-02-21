7043a0adb9d034030c04b562ff419dfb8f07863f
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-02-21 09:58:32 +0300

    Time: 74 ms (45.42%), Space: 13.9 MB (78.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,27 +5,63 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = num2 = ""
-        
         cur1 = l1
         cur2 = l2
+        newCur = newHead = ListNode(-1)
+        rem = 0
         
+        while cur1 and cur2:
+            
+            q = ((cur1.val+cur2.val+rem)%10)
+            rem = (cur1.val+cur2.val+rem)//10
+            node = ListNode(q)
+            newCur.next = node
+            newCur = newCur.next
+            cur1 = cur1.next
+            cur2 = cur2.next
+            
         while cur1:
-            num1 = str(cur1.val) + num1
+            q = ((cur1.val+rem)%10)
+            rem = (cur1.val+rem)//10
+            node = ListNode(q)
+            newCur.next = node
+            newCur = newCur.next
             cur1 = cur1.next
             
         while cur2:
-            num2 = str(cur2.val) + num2
+            q = ((cur2.val+rem)%10)
+            rem = (cur2.val+rem)//10
+            node = ListNode(q)
+            newCur.next = node
+            newCur = newCur.next
             cur2 = cur2.next
-        
-        summ = str(int(num1) + int(num2))
-        
-        newHead = ListNode(int(summ[-1]))
-        cur = newHead
-        
-        for i in range(len(summ)-2,-1,-1):
-            node = ListNode(summ[i])
-            cur.next = node
-            cur = cur.next
             
-        return newHead
+        if rem:
+            newCur.next = ListNode(rem)
+        
+        return newHead.next
+        
+#         num1 = num2 = ""
+        
+#         cur1 = l1
+#         cur2 = l2
+        
+#         while cur1:
+#             num1 = str(cur1.val) + num1
+#             cur1 = cur1.next
+            
+#         while cur2:
+#             num2 = str(cur2.val) + num2
+#             cur2 = cur2.next
+        
+#         summ = str(int(num1) + int(num2))
+        
+#         newHead = ListNode(int(summ[-1]))
+#         cur = newHead
+        
+#         for i in range(len(summ)-2,-1,-1):
+#             node = ListNode(summ[i])
+#             cur.next = node
+#             cur = cur.next
+            
+#         return newHead
