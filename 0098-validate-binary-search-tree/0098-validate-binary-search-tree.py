d7f2dca48fa53bd38ac16c55c9ca04bcf5b72e13
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-07 16:33:40 +0300

    Time: 52 ms (34.26%), Space: 17.1 MB (11.95%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,30 +5,61 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode],isTop=True) -> bool:
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.res = True
         
-        left = right = []
         
-        if not root.left and not root.right:
-            return [root.val]
-        
-        if root.left:
-            left = self.isValidBST(root.left,False)
+        def helper(root):
+            # (min,max)
+            if not root:
+                return (inf,-inf)
+
+            leftMin,leftMax = helper(root.left)
+            rightMin,rightMax = helper(root.right)
             
-        if root.right:
-            right = self.isValidBST(root.right,False)
+            val = leftMax < root.val < rightMin
             
-            
-        if isTop:
-            res = left + [root.val] + right
-            return self.isSorted(res)
+            if not val and self.res:
+                self.res = val
+                
+            return (min(root.val,leftMin),max(root.val,rightMax))
         
-        return left + [root.val] + right
+        helper(root)
+        
+        return self.res
+            
+            
+
+
+        
+            
+        
+            
+        
+        
+#         left = right = []
+        
+#         if not root.left and not root.right:
+#             return [root.val]
+        
+#         if root.left:
+#             left = self.isValidBST(root.left,False)
+            
+#         if root.right:
+#             right = self.isValidBST(root.right,False)
+            
+            
+#         if isTop:
+#             res = left + [root.val] + right
+#             return self.isSorted(res)
+        
+#         return left + [root.val] + right
     
     
-    def isSorted(self,arr):
-        for i in range(1,len(arr)):
-            if arr[i]<=arr[i-1]:
-                return False
+#     def isSorted(self,arr):
+#         for i in range(1,len(arr)):
+#             if arr[i]<=arr[i-1]:
+#                 return False
             
-        return True
+#         return True
