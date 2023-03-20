c5bf6d7f5cd08eb0922555a0a7089240198068c2
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-20 17:57:50 +0300

    Time: 245 ms (56.77%), Space: 27.6 MB (5.15%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -5,9 +5,11 @@ class Solution:
         self.unique = set()
         def backtrack(path,nums):
             
-            if len(path)>1 and tuple(path) not in self.unique:
-                self.subSeqs.append(path)
-                self.unique.add(tuple(path))
+            if len(path)>1:
+                tp = tuple(path)
+                if tp not in self.unique:
+                    self.subSeqs.append(path)
+                    self.unique.add(tp)
             
             for i in range(len(nums)):
                 if not path or path[-1] <= nums[i]:
