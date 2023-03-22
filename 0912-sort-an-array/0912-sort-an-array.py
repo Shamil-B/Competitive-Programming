1765b676222f30662b3fc52c642a14f91ad05d07
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-22 18:53:55 +0300

    Time: 2209 ms (22.85%), Space: 35.9 MB (5.05%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -18,18 +18,18 @@ class Solution:
     def merge(self,arr1,arr2):
         
         p1 = p2 = 0
-        mergedArr = []
         n = len(arr1)
         m = len(arr2)
+
         while p1<n or p2<m:
             
             if p2 == m or (p1<n and arr1[p1] <= arr2[p2]):
-                mergedArr.append(arr1[p1])
+                arr1.append(arr1[p1])
                 p1 += 1
                 
             elif p1 == n or (p2<m and arr1[p1] > arr2[p2]):
-                mergedArr.append(arr2[p2])
+                arr1.append(arr2[p2])
                 p2 += 1
                 
                 
-        return mergedArr
+        return arr1[n:]
