be1e639a9ade3931196d58e4e04b3c38b2d82019
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-23 17:59:58 +0300

    Time: 162 ms (27.33%), Space: 15.2 MB (29.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maxx = len(nums)+1
-        dic = {}
+        nums += [-1]
         
-        for num in nums:
-            dic[num] = 1
-        
-        for i in range(maxx):
-            if i not in dic:
-                return i
+        ind = 0
+        n = len(nums)
+        while ind<n:
+            corPos = nums[ind]
             
-            
+            if corPos != ind and nums[ind] !=-1:
+                nums[corPos],nums[ind] = nums[ind],nums[corPos]
+                
+            else:
+                ind += 1
+                
+        misInd = 0
+        for index,num in enumerate(nums):
+            if num == -1:
+                misInd = index
+                
+        return misInd
+                
