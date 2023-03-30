74ab35121a4a8dda7014d65dd18d701863170fb6
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-30 13:54:36 +0300

    Time: 47 ms (24.58%), Space: 14.1 MB (49.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,24 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.permutations = []
+        permutations = []
         n = len(nums)
         setWithBit = 0
         candidates = [nums[i] for i in range(n)]
         target = 2**n-1
-        def backtrack(path):
-            nonlocal setWithBit,target
+        path = []
+        def backtrack():
+            nonlocal setWithBit,target,permutations,path
 
             if target == setWithBit:
-                self.permutations.append(path[:])
+                permutations.append(path[:])
                 return
 
             for i in range(n):
                 if 1<<i&setWithBit==0:
                     path.append(candidates[i-1])
                     setWithBit |= 1<<i
-                    backtrack(path[:])
+
+                    backtrack()
+
                     path.pop()
                     setWithBit ^= 1<<i
         
-        backtrack([])
-        return self.permutations
+        backtrack()
+        return permutations
