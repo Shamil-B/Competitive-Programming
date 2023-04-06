bf8b795b82bbfaf22ad9c50ac793e5d4f960ba3f
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-04-06 17:23:40 +0300

    Time: 785 ms (93.98%), Space: 49.9 MB (37.59%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,22 +1,3 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        freq = {}
-        n = len(edges)
-
-        for edge in edges:
-            if edge[0] not in freq:
-                freq[edge[0]] = 1
-                
-            else:
-                freq[edge[0]] += 1
-                
-            if edge[1] not in freq:
-                freq[edge[1]] = 1
-                
-            else:
-                freq[edge[1]] += 1
-                
-                
-        for item in freq.items():
-            if item[1] == n:
-                return item[0]
+        return edges[0][0] if (edges[0][0]==edges[1][0] or edges[0][0]==edges[1][1]) else edges[0][1]
