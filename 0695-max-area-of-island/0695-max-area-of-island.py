52014f67816cb08ec69cc4645befe9d910060244
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-04-10 13:55:09 +0300

    Time: 9044 ms (5.04%), Space: 18 MB (6.14%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        graph = {}
+        n = len(grid)
+        m = len(grid[0])
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    graph[(i,j)] = []
+                    if i>0 and grid[i-1][j]:
+                        graph[(i,j)].append((i-1,j))
+                    if j>0 and grid[i][j-1]:
+                        graph[(i,j)].append((i,j-1))
+                        
+                    if i<n-1 and grid[i+1][j]:
+                        graph[(i,j)].append((i+1,j))
+                        
+                    if j<m-1 and grid[i][j+1]:
+                        graph[(i,j)].append((i,j+1))
+                    
+        def dfs(node,graph,visited=None):
+            if not visited:
+                visited = set()
+            
+            visited.add(node)
+            for neigh in graph[node]:
+                if neigh not in visited:
+                    dfs(neigh,graph,visited)
+
+            return len(visited)
+
+        max_ = 0   
+        for key in graph.keys():
+            res = dfs(key,graph)
+            max_ = max(max_,res)
+            
+            
+        return max_
+            
+        
