b34d6abe4241478167bf195153cd21c513f64df7
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-04-27 18:19:13 +0300

    Time: 84 ms (5.78%), Space: 16.8 MB (10.24%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -3,14 +3,14 @@ class Solution:
         n = len(rooms)
         visited = set()
 
-        stack = [0]
-        while stack:
-            room = stack.pop()
+        q = deque([0])
+        while q:
+            room = q.popleft()
             if room in visited:
                 continue
                 
             visited.add(room)
             for neigRoom in rooms[room]:
-                stack.append(neigRoom)
+                q.append(neigRoom)
 
         return len(visited) == len(rooms)
