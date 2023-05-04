8e610d115ee5dcb5b28aa080fcc4d0f108231a44
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-05-04 17:44:01 +0300

    Added solution - LeetHub

--- Heap Sort - GFG/heap-sort.py
+++ Heap Sort - GFG/heap-sort.py
@@ -1,28 +1,79 @@
-#User function Template for python3
+# User function Template for python3
 
 class Solution:
-    #Heapify function to maintain heap property.
-    def heapify(self,arr, n, i):
+    # Heapify function to maintain heap property.
+    def heapifyDown(self, arr, n, i):
         current = i
-        
-        parent = (current-1)//2
-        while parent>=0 and arr[parent]>arr[current]:
-            arr[current],arr[parent] = arr[parent],arr[current]
-            current = parent
-            parent = (current-1)//2
-            
-        return arr
-            
-    
-    #Function to build a Heap from array.
-    def buildHeap(self,arr,n):
-        return arr
-    
-    #Function to sort an array using Heap Sort.    
-    def HeapSort(self, arr, n):
-       arr.sort()
-       return arr
 
+        while True:
+            left = (2*current+1)
+            right = (2*current+2)
+
+            if left >= n:
+                left = None
+
+            if right >= n:
+                right = None
+
+            if left and right:
+                if arr[left] < arr[right]:
+                    if arr[left] < arr[current]:
+                        arr[left], arr[current] = arr[current], arr[left]
+                        current = left
+
+                    else:
+                        return arr
+
+                else:
+                    if arr[right] < arr[current]:
+                        arr[right], arr[current] = arr[current], arr[right]
+                        current = right
+
+                    else:
+                        return arr
+
+            elif right:
+                if arr[right] < arr[current]:
+                    arr[right], arr[current] = arr[current], arr[right]
+                    current = right
+
+                else:
+                    return arr
+
+            elif left:
+                if arr[left] < arr[current]:
+                    arr[left], arr[current] = arr[current], arr[left]
+                    current = left
+
+                else:
+                    return arr
+
+            else:
+                return arr
+
+    # Function to build a Heap from array.
+    def buildHeap(self, arr, n):
+        heap = arr.copy()
+        for ind in range(n-1, -1, -1):
+            heap = self.heapifyDown(heap, n, ind)
+
+        return heap
+
+    # Function to sort an array using Heap Sort.
+    def HeapSort(self, arr, n):
+        tmpArr = arr.copy()
+
+        heap = self.buildHeap(tmpArr, n)
+        i = 0
+
+        while heap:
+            heap[0], heap[-1] = heap[-1], heap[0]
+            arr[i] = heap.pop()
+            heap = self.heapifyDown(heap, len(heap), 0)
+            i += 1
+            
+        return arr
+    
 
 #{ 
  # Driver Code Starts
