7208656e5d393b9dd5f68330ea22313cc4af421c
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-05-04 18:49:08 +0300

    Time: 76 ms (6.81%), Space: 16.4 MB (6.37%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+        newFreq = {}
+
+        for item in freq.items():
+            if item[1] in newFreq:
+                newFreq[item[1]].append(item[0])
+                
+            else:
+                newFreq[item[1]] = [item[0]]
+                
+        for key in newFreq.keys():
+            newFreq[key] = sorted(newFreq[key])
+
+        newFreq = list(newFreq.items())
+        newFreq.sort(reverse=True)
+        ans = []
+        
+        for i in range(len(newFreq)):
+            for j in range(len(newFreq[i][1])):
+                ans.append(newFreq[i][1][j])
+                if len(ans)==k:
+                    return ans
