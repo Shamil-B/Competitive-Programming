21f3610ae681758e9613aa4f584d709c59532346
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-05-18 17:54:16 +0300

    Time: 1851 ms (34.11%), Space: 65.8 MB (74.51%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        # we do the union find
+        
+        self.representatives = {i:i for i in range(1,n+1)}
+        self.heights = {i:1 for i in range(1,n+1)}
+        self.costs = {i:float(inf) for i in range(1,n+1)}
+            
+        # now for every edge we are going to construct the union graph
+        for city1,city2,cost in roads:
+            self.union(city1,city2,cost)
+
+        return self.costs[self.find(1)]
+    
+    def find(self,x):
+        rep = x
+        while self.representatives[rep]!=rep:
+            rep = self.representatives[rep]
+
+        topRep = rep
+        rep = x
+
+        while self.representatives[rep]!=rep:
+            curNode = rep
+            rep = self.representatives[rep]
+            self.representatives[curNode] = topRep
+
+        return topRep
+        
+    def union(self,x,y,cost):
+        height1 = self.heights[x]
+        height2 = self.heights[y]
+
+        newHeight = max(height1,height2)
+
+        if height1 == height2:
+            newHeight = height1+1
+
+        if height1 > height2:
+            newRep = self.find(x)
+            oldRep = self.find(y)
+
+        else:
+            newRep = self.find(y)
+            oldRep = self.find(x)
+
+        self.representatives[oldRep] = newRep
+        self.costs[newRep] = min(self.costs[newRep],self.costs[oldRep],cost)
