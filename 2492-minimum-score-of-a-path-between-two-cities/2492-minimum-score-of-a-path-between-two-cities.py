70ec222b146badde6bd776e23faa551c796ef9b6
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-05-18 17:59:28 +0300

    Time: 1921 ms (28.64%), Space: 65.7 MB (74.65%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -4,14 +4,19 @@ class Solution:
         
         self.representatives = {i:i for i in range(1,n+1)}
         self.heights = {i:1 for i in range(1,n+1)}
+        
+        # the only additional lines to the normal union find template are 3
+        # ----------------here--------------#
+
         self.costs = {i:float(inf) for i in range(1,n+1)}
-            
+
         # now for every edge we are going to construct the union graph
         for city1,city2,cost in roads:
             self.union(city1,city2,cost)
 
+        # ----------------here--------------#
         return self.costs[self.find(1)]
-    
+
     def find(self,x):
         rep = x
         while self.representatives[rep]!=rep:
@@ -45,4 +50,6 @@ class Solution:
             oldRep = self.find(x)
 
         self.representatives[oldRep] = newRep
+        
+        # ----------------and here--------------#
         self.costs[newRep] = min(self.costs[newRep],self.costs[oldRep],cost)
