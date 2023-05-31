8274f7e1f953673ae55de2aa467c22ab56abbf47
Shamil <shamilbedru47@gmail.com>
2023-05-31 18:57:08 +0300

    Time: 49 ms (26.05%), Space: 16.3 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,6 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def solve(cord=(0,0),memo={}):
+        def topDownDP(cord=(0,0),memo={}):
             
             row,col = cord
             if row==m-1 and col==n-1:
@@ -10,12 +10,32 @@ class Solution:
             if (row,col) in memo:
                 return memo[(row,col)]
             if row+1<m:
-                ways+=solve((row+1,col),memo)
+                ways+=topDownDP((row+1,col),memo)
                 
             if col+1<n:
-                ways += solve((row,col+1),memo)
+                ways += topDownDP((row,col+1),memo)
                 
             memo[(row,col)] = ways
             return memo[(row,col)]
         
-        return solve()
+        # return topDownDP()
+
+        #row wise
+        def bottomUpDP():
+            start = (m-1,n-1)
+            dp = [[0 for i in range(n)] for i in range(m)]
+            dp[-1][-1] = 1 
+            
+            
+            for col in range(n-1,-1,-1):
+                for row in range(m-1,-1,-1):
+                    dp[row][col] += (self.calculate(row+1,col,m,n,dp)+self.calculate(row,col+1,m,n,dp))
+                
+            return dp[0][0]
+
+        return bottomUpDP()
+    
+    def calculate(self,x,y,m,n,dp):
+        if 0<=x<m and 0<=y<n:
+            return dp[x][y]
+        return 0
