921addc8b37a2ec08400efd0f6565a371140d6b6
Shamil <shamilbedru47@gmail.com>
2023-06-22 11:08:19 +0300

    Time: 5157 ms (50.80%), Space: 32.1 MB (24.19%) - LeetHub

---
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -0,0 +1,20 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def createSortedArray(self, instructions: List[int]) -> int:
+        cost = 0
+        sortedList = SortedList()
+
+        for num in instructions:
+            cost += min(sortedList.bisect_left(num), len(sortedList) - sortedList.bisect_right(num))
+            cost %= (10**9 + 7)
+            sortedList.add(num)
+
+        return cost
+        
+    def calculateCost(self,nums,num):
+        leftCost = bisect_left(nums,num)
+        rightCost = len(nums) - bisect_right(nums,num)
+
+        return min(leftCost,rightCost)     
+            
