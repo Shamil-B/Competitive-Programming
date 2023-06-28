118e8246c198a2d0174f6baa1e9aab01e8df2587
Shamil <shamilbedru47@gmail.com>
2023-06-28 18:25:43 +0300

    Time: 157 ms (5.15%), Space: 22.3 MB (10.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
+        
         def getDown(cord):
             if cord[0]<len(grid)-1:
                 return (grid[cord[0]+1][cord[1]])
@@ -15,25 +16,44 @@ class Solution:
         
         # bottom up
        
-        for row in range(len(grid)-1,-1,-1):
-            for col in range(len(grid[0])-1,-1,-1):
+        def bottomUp():
+            for row in range(len(grid)-1,-1,-1):
+                for col in range(len(grid[0])-1,-1,-1):
 
-                down = getDown((row,col))
-                right = getRight((row,col))
+                    down = getDown((row,col))
+                    right = getRight((row,col))
 
-                if down != -1 and right != -1:
-                    grid[row][col] += min(down,right)
+                    if down != -1 and right != -1:
+                        grid[row][col] += min(down,right)
 
-                elif down != -1:
-                    grid[row][col] += down
+                    elif down != -1:
+                        grid[row][col] += down
 
-                elif right != -1:
-                    grid[row][col] += right
-             
-        return grid[0][0]
+                    elif right != -1:
+                        grid[row][col] += right
 
+            return grid[0][0]
+
+        # topDown
+        def topDown(i,j):
+
+            if i >= len(grid) or j >= len(grid[0]):
+                return inf
             
+            if (i,j) == (len(grid)-1,len(grid[0])-1):
+                return grid[-1][-1]
             
+            state = (i,j)
             
-            
+            if state not in memo:
+                memo[state] = grid[i][j] + min(topDown(i+1,j),topDown(i,j+1))
+                
+            return memo[state]
         
+        memo = {}
+        return topDown(0,0)
+
+
+
+
+
