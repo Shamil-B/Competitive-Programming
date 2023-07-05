2e54d9d4e34b8d978d710cc20ef8dcb1fe77d616
Shamil <shamilbedru47@gmail.com>
2023-07-05 23:40:36 +0300

    Time: 54 ms (80.47%), Space: 16.4 MB (38.90%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        
+        n = len(satisfaction)
+        satisfaction.sort()
+
+        positiveStart = 0
+        for i in range(n):
+            if satisfaction[i] > 0:
+                positiveStart = i
+                break
+
+        reserveSum = sum(satisfaction[positiveStart:])
+        negSum = sum(satisfaction[:positiveStart])
+        finalSum = 0
+
+        j = 1
+        for i in range(n):
+            if satisfaction[i] < 0:
+                negSum -= satisfaction[i]
+                value = satisfaction[i]*(j) + negSum
+                
+            else:
+                value = satisfaction[i]*(j)
+
+            if value + reserveSum > 0:
+                finalSum += satisfaction[i]*(j)
+                j += 1
+                
+        return finalSum
