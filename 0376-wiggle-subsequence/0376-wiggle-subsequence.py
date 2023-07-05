f5a791e4b6c0603049e37c52e186d0548e2a7045
Shamil <shamilbedru47@gmail.com>
2023-07-05 21:21:51 +0300

    Time: 45 ms (87.50%), Space: 16.4 MB (25.00%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -3,11 +3,6 @@ class Solution:
         if len(nums) == 1 or len(set(nums)) == 1:
             return 1
 
-        if len(nums) == 2:
-            if nums[0] != nums[1]:
-                return 2
-            return 1
-
         max_ = min_ = nums[0]
         diff = nums[1]
         i = 2
