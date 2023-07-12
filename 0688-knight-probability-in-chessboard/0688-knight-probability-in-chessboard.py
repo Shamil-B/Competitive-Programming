c77f61748801b26f3dd6ff53ca425abec33da79f
Shamil <shamilbedru47@gmail.com>
2023-07-12 16:13:19 +0300

    Time: 297 ms (49.32%), Space: 26.7 MB (11.11%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,22 +1,19 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        @cache
         def solve(cord,k):
             if self.checkCord(cord,n) == 0 or k < 0:
                 return 0
 
             valids = 0
             nextMoves = [(-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1)]
-            state = (cord,k)
-            if state not in memo:
-                for move in nextMoves:
-                    valids += solve((cord[0]+move[0],cord[1]+move[1]),k-1)
-                    
-                
-                memo[state] = valids + (1 if k == 0 else 0)
-                
-            return memo[state]
+
+            for move in nextMoves:
+                valids += solve((cord[0]+move[0],cord[1]+move[1]),k-1)
+
+            valids += (1 if k == 0 else 0)
+            return valids
             
-        memo = {}
         res = solve((row,column),k)
         return res/(8**k)
             
