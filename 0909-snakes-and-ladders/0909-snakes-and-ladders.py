039a117a5621f5659963579462feefa2ccf47fcf
Shamil <shamilbedru47@gmail.com>
2023-07-22 14:29:44 +0300

    Time: 393 ms (5.01%), Space: 16.6 MB (22.37%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,39 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        shortcut = {}
+        n = len(board)
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] != -1:
+                    row = n-1-i
+                    if row % 2:
+                        col = n-1-j+1
+                    else:
+                        col = j+1
+
+                    num = row*n + col
+                    shortcut[num] = board[i][j] 
+                    
+        q = [(0,1,False)]
+        heapq.heapify(q)
+        visited = defaultdict(int)
+
+        while q:
+            level, cur, tookRide = heapq.heappop(q)
+
+            if cur >= n**2:
+                return level
+            
+            
+            if cur in shortcut and not tookRide:
+                heapq.heappush(q,(level, shortcut[cur],True))
+   
+            else:
+                if visited[cur] > 2:
+                    continue
+                
+                visited[cur] += 1
+
+                for i in range(1,7):
+                    heapq.heappush(q,(level+1, cur+i,False))
+        return -1
