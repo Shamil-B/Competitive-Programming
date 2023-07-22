7abf509a767d783c1b47a1a2d05bfa870c673b32
Shamil <shamilbedru47@gmail.com>
2023-07-22 15:43:05 +0300

    Time: 3793 ms (8.28%), Space: 358.3 MB (7.15%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,6 +1,7 @@
 class Solution:
     def racecar(self, target: int) -> int:  
-        # choose either A or R and return when u get to target
+        # choose either A or R for each step and return the length of the instruction
+        # when u get to target
         # the first to get there is the answer
         
         position = 0
@@ -21,5 +22,6 @@ class Solution:
             visited.add((position,speed))
 
             q.append((position+speed, speed*2, instructionLength+1))
-
-            q.append((position, (speed//abs(speed))*-1, instructionLength+1))
+            
+            speed = -1 if speed > 0 else 1
+            q.append((position, speed, instructionLength+1))
