2f6b0937e936ee620c4c356271ac34856535071c
Shamil <shamilbedru47@gmail.com>
2023-08-18 16:01:34 +0300

    Time: 354 ms (17.09%), Space: 56.5 MB (20.05%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        def solve(day,have_stock,memo,stocks):
+            if day >= len(prices):
+                return 0
+            
+            state = (day,have_stock,stocks)
+            if state in memo:
+                return memo[state]
+            
+            if have_stock and stocks == k:
+                res = max(prices[day],solve(day+1,True,memo,stocks))
+                memo[state] = res
+                return memo[state]
+            
+            elif have_stock:
+                sell = solve(day+1,False,memo,stocks) + prices[day]
+                avoid = solve(day+1,True,memo,stocks)
+                res = max(sell,avoid)
+                memo[state] = res
+                return memo[state]
+
+            else:
+                buy = solve(day+1,True,memo,stocks+1)-prices[day]
+                avoid = solve(day+1,False,memo,stocks)
+                res = max(buy,avoid)
+                memo[state] = res
+                return memo[state]
+        memo = {}
+        return solve(0,False,memo,0)
