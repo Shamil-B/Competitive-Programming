7b7a76bd2d4aadf50d1442faffae937357763c78
Shamil <shamilbedru47@gmail.com>
2023-09-11 21:02:36 +0300

    Time: 61 ms (7.91%), Space: 28.1 MB (5.06%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
         @cache
         def solve(ind,word):
             if ind >= len(s):
                 if not word or word in wordDict:
                     return True
-                
                 return False
-            
+
             if word in wordDict:
                 return solve(ind+1,s[ind]) or solve(ind+1,word+s[ind])
-            
+
             return solve(ind+1,word+s[ind])
         
         return solve(0,"")
