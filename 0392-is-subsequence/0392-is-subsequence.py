e92a2306a20da374df9e71bd4b91cc999ef8893a
Shamil <shamilbedru47@gmail.com>
2023-09-23 23:45:24 +0300

    Time: 38 ms (68.61%), Space: 16.3 MB (79.81%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -11,4 +11,21 @@ class Solution:
 
             return solve(ind+1,word) or solve(ind+1,word+t[ind])
         
-        return solve(0,"")
+        # return solve(0,"")
+
+        p1 = p2 = 0
+        for i in range(size):
+            if p1 >= len(s):
+                break
+
+            if s[p1] == t[p2]:
+                p1 += 1
+                p2 += 1
+                
+            else:
+                p2 += 1
+                
+        if p1 == len(s):
+            return True
+        
+        return False
