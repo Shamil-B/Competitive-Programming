5cb8e1e1536eeff33d627fc52fb0427f0b538caa
Shamil <shamilbedru47@gmail.com>
2023-09-25 11:38:09 +0300

    Time: 2376 ms (11.87%), Space: 18.4 MB (13.37%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -0,0 +1,29 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        size = len(scores)
+        teams = []
+        for i in range(size):
+            teams.append((ages[i], scores[i]))
+
+        teams.sort()
+        
+        def isValidAction(ind1,ind2):
+            if teams[ind1][0] < teams[ind2][0] and teams[ind1][1] > teams[ind2][1]:
+                return False
+
+            return True
+
+        @cache
+        def solve(ind):
+            max_ = teams[ind][1]
+            for i in range(ind+1, size):
+                if isValidAction(ind,i):
+                    max_ = max(max_, solve(i) + teams[ind][1])
+
+            return max_
+
+        max_ = 0
+        for i in range(size):
+            max_ = max(max_, solve(i))
+
+        return max_
