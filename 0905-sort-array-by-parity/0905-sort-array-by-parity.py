26b5fa0fb4b644976b70a1e7809f549bf2ea74cb
Shamil <shamilbedru47@gmail.com>
2023-09-28 22:23:00 +0300

    Time: 86 ms (12.42%), Space: 16.9 MB (96.40%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        evens  = []
+        odds = []
+        
+        for num in nums:
+            if num % 2:
+                odds.append(num)
+            else:
+                evens.append(num)
+                
+        return evens + odds
