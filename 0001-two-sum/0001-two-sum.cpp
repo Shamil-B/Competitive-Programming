83d66ba63f5ddc996b599b6cd13813a6324d403b
Shamil <shamilbedru47@gmail.com>
2023-10-09 21:20:50 +0300

    Time: 8 ms (78.39%), Space: 11.2 MB (20.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-//         int p1 = 0;
-        int size = nums.size(); // Use nums.size() to get the size of the vector
-//         int p2 = size-1;
-//         sort(nums.begin(), nums.end());
+        int p1 = 0;
+        int size = nums.size();
+        int p2 = size - 1;
+        vector<pair<int, int>> newNums = {};
         
-        
+        for (int i = 0; i < size; i++) {
+            newNums.push_back({nums[i], i});
+        }
+        sort(newNums.begin(), newNums.end());
 
-//         while(p2 > p1){
-//             int sum = nums[p2] + nums[p1];
-//             if(sum == target){
-//                 vector<int> ans = {p1, p2};
-//                 return ans;
-//             }
-            
-//             else if(sum < target){
-//                 p1 = p1 + 1;
-//             }
-//             else{
-//                 p2 = p2 - 1;
-//             }
-//         }
-
-//         // Handle the case when no valid pair is found
-//         return {}; // Return an empty vector to indicate no solution
-        
-        for(int i = 0; i < size; i++){
-            for(int j = size-1; j > 0; j--){
-                if((nums[i] + nums[j] == target) & (i != j)){
-                    return {i, j};
-                }
+        while (p2 > p1) {
+            int sum = newNums[p2].first + newNums[p1].first;
+            if (sum == target) {
+                vector<int> ans = {newNums[p1].second, newNums[p2].second};
+                return ans;
+            } else if (sum < target) {
+                p1 = p1 + 1;
+            } else {
+                p2 = p2 - 1;
             }
         }
-        return {};
+
+        // Handle the case when no valid pair is found
+        return {}; // Return an empty vector to indicate no solution
     }
 };
