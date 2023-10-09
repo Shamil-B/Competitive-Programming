9c0f4841cee5796f0fbb0491fd15b49543a07c00
Shamil <shamilbedru47@gmail.com>
2023-10-09 17:55:13 +0300

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.py

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,35 +1,41 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        splitted = self.splitIt(haystack,len(needle))
-        
-        if needle in splitted:
-            return splitted[needle]
-        
-        return -1
-    
-    
-    def splitIt(self,string,size):
-        
-        index = 0
-        resDic = {}
-        word = ''
-        
-        for ind,ch in enumerate(string):
-            if ind < size-1:
-                word += ch
-                
-            elif ind == size-1:
-                word += ch
-                resDic[word] = index
-                index += 1
-                
-            else:
-                word += ch
-                word = word[1:]
-                if word not in resDic:
-                    resDic[word] = index
-                index += 1
-                
-                
-        return resDic
+        alpha = 27
+        size1 = len(haystack)
+        size2 = len(needle)
+        size = max(len(haystack),len(needle))
+        hashes = []
+
+        for i in range(size):
+            hashes.append(pow(alpha,i))
             
+        def addLast(wordHash, newCh):
+            return wordHash*alpha + (ord(newCh)-ord('a')+1)
+
+        def pollFirst(wordHash, firstCh, oldLength):
+            return wordHash - (ord(firstCh)-ord('a')+1)*hashes[oldLength-1]
+
+        def compareHash(hash1, hash2):
+            return hash1 == hash2
+
+        def solve(word, target):
+            targetHash = 0
+            for ch in target:
+                targetHash = addLast(targetHash, ch)
+
+            currentHash = 0
+            for ch in word[:size2]:
+                currentHash = addLast(currentHash, ch)
+
+            window_size = size2
+            for i in range(size1):
+                if compareHash(currentHash, targetHash):
+                    return i
+
+                if i+window_size < size1 and i < size1:
+                    currentHash = pollFirst(currentHash, word[i], size2)
+                    currentHash = addLast(currentHash, word[i + window_size])
+
+            return -1
+        
+        return solve(haystack, needle)
