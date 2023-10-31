c21d9c36fbcd1fd7cc28adcee9f2fc7e6c6b3385
Shamil <shamilbedru47@gmail.com>
2023-10-31 17:32:38 +0300

    Time: 67 ms (37.05%), Space: 16.7 MB (39.19%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
         self.ans = []
-        self.set_ = set()
         size = len(nums)
         def backtrack(arr, path):
-            if len(path) >= size and tuple(path) not in self.set_:
+            if len(path) >= size:
                 self.ans.append(path)
-                self.set_.add(tuple(path))
                 return
 
+            used = []
             for i in range(len(arr)):
-                new_arr = arr[:]
-                new_arr.remove(arr[i])
-                backtrack(new_arr, path[:]+[arr[i]])
+                if arr[i] not in used:
+                    used.append(arr[i])
+                    new_arr = arr[:]
+                    new_arr.remove(arr[i])
+                    backtrack(new_arr, path[:]+[arr[i]])
                 
         backtrack(nums[:], [])
         return self.ans
